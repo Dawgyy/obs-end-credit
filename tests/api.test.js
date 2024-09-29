45edd8633e709146371bb75f0b37ff1c0ef86589
Dawgy <dxwgy.contact@gmail.com>
2024-09-29 14:43:48 +0200

    Fix dev/prod deploy

--- tests/api.test.js
+++ tests/api.test.js
@@ -17,14 +17,14 @@ afterAll((done) => {
 
 describe('GET /api/subs', () => {
     it('should return list of subscribers', async () => {
-        const response = await request(server).get('/api/subs'); // Utiliser 'server' au lieu de 'app'
+        const response = await request(server).get('/api/subs'); 
         expect(response.statusCode).toBe(200);
     });
 });
 
 describe('GET /api/followers', () => {
     it('should return list of followers', async () => {
-        const response = await request(server).get('/api/followers'); // Utiliser 'server' au lieu de 'app'
+        const response = await request(server).get('/api/followers');
         expect(response.statusCode).toBe(200);
     });
 });
